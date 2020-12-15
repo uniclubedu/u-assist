e13b12b5ae5191fe3ab13172e6389df71f50efdd
Sharma <tarunx.sharma@intel.com>
2020-12-15 13:52:14 +0530

    Initial Base Check-in

---
+++ src/test/java/com/intel/cloud/byoc/Application/ByocApplication.java
@@ -0,0 +1,16 @@
+package com.intel.cloud.byoc.Application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ByocApplication {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
