d7b8c1fe4f557dde1bf52b9205b64ba1206d7849
Sharma <tarunx.sharma@intel.com>
2020-12-16 17:05:27 +0530

    removing outer classes

--- dev-CRUD/src/main/java/com/intel/cloud/byoc/service/ConfigurationTabService.java
+++
@@ -1,9 +0,0 @@
-package com.intel.cloud.byoc.service;
-
-import java.util.List;
-import com.intel.cloud.byoc.model.ConfigurationTab;
-
-public interface ConfigurationTabService {
-
-public List<ConfigurationTab> getConfiguration() ;
-}
